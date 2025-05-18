a25876bfb16c489a17e607d26702762e92cd8d6b
Ayman Eldawy <aymaneldawy@yahoo.com>
2025-05-18 22:50:24 +0300

    fix pagination

--- src/services/paginationService.js
+++ src/services/paginationService.js
@@ -2,16 +2,7 @@ import http from "./http";
 import API_URL_CONSTANTS from "./APIUrlConstants";
 
 export const getOneBy = async (entity, value, column = "number") => {
-  console.log(`entity: ${entity}, value: ${value}, column: ${column}`);
-
-  const params = {
-    entity: 'bank',
-    value,
-    column,
-  }
-  // Object.keys(params).forEach(key => params[key] === undefined && delete params[key]);
-
-  return http.post(`/pagination/one-by`, { ...params });
+  return http.post(`/pagination/one-by`, { entity, value, column });
 };
 
 export const getNextOne = async (entity, value, columns = ["*"], code) => {
@@ -19,25 +10,15 @@ export const getNextOne = async (entity, value, columns = ["*"], code) => {
 };
 
 export const getPreviousOne = async (entity, value, columns = ["*"], code) => {
-  getNextOne(entity, value, columns, code);
   return http.post(`/pagination/previous-one`, { entity, value: +value, code });
 };
 
 export const getFirstOne = async (entity, columns = ["*"], code) => {
-  let params = {
-    entity
-  }
-  if (code) params.code = code;
-  return http.post(`/pagination/first-one`, { ...params });
+
+  return http.post(`/pagination/first-one`, { entity, code });
 };
 
 export const getLastOne = async (entity, columns = ["*"], code) => {
-  console.log(`entity: ${entity}, columns: ${columns}, code: ${code}`);
-
-  let params = {
-    entity: 'bank',
-  }
-  if (code) params.code = code;
-  return http.post(`/pagination/last-one`, { ...params });
+  return http.post(`/pagination/last-one`, { entity, code });
 };
 
