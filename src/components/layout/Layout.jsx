159c85fdb2979d4002b8ed4a9f2e62a2a7ddcbe1
Ayman Eldawy <aymaneldawy@yahoo.com>
2025-05-18 22:35:51 +0300

    set dynamic menu links

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,15 +6,26 @@ import sideMenuItems from '@/data/sideMenuItems'
 import Sidebar from './Sidebar'
 import withLoggedOut from '@/HOC/withLoggedOut'
 import { useMenuData } from '@/hook/useMenuData'
+import { useMemo } from 'react'
 
 const Layout = () => {
-  // const menu = useMenuData()
- 
+  const { data, refetch, isLoading } = useMenuData();
+  
+  const menu = useMemo(() => {
+    return sideMenuItems.map((item) => {
+      if (item.key === 'Accounting_Transactions') {
+        item.children = data;
+      }
+      return item;
+    })
+  }, [data, isLoading])
+  console.log(menu, 'data', isLoading);
+
   return (
     <div id="layout-wrapper" className={"flex flex-col pb-12 min-h-screen"}>
       <Header />
-      <Menu menu={sideMenuItems} />
-      <Sidebar menu={sideMenuItems} />
+      <Menu menu={isLoading ? [] : menu} />
+      <Sidebar menu={isLoading ? [] : menu} />
       <Outlet />
       <Footer />
     </div>
