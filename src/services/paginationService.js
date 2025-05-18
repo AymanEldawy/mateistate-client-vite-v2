4c77aa5fb70e0a55d5c39ae3583f0f42c4d4bec4
Ayman Eldawy <aymaneldawy@yahoo.com>
2025-05-18 22:36:57 +0300

    fix pagination

--- src/services/paginationService.js
+++ src/services/paginationService.js
@@ -1,43 +1,43 @@
 import http from "./http";
 import API_URL_CONSTANTS from "./APIUrlConstants";
 
-export const getOneBy = async (entity, value, column = "id", columns = ["*"]) => {
-  const params = new URLSearchParams();
-  if (entity) params.append("entity", entity);
-  if (value) params.append("value", value);
-  if (column) params.append("column", column);
-  if (columns.length) params.append("columns", columns.join(","));
+export const getOneBy = async (entity, value, column = "number") => {
+  console.log(`entity: ${entity}, value: ${value}, column: ${column}`);
 
-  return http.get(`${API_URL_CONSTANTS.BASE_ACCOUNT}?${params.toString()}`);
+  const params = {
+    entity: 'bank',
+    value,
+    column,
+  }
+  // Object.keys(params).forEach(key => params[key] === undefined && delete params[key]);
+
+  return http.post(`/pagination/one-by`, { ...params });
 };
 
 export const getNextOne = async (entity, value, columns = ["*"], code) => {
-   return http.post(`pagination/next-one`, {
-    entity,
-    value,
-    code,
-  });
+  return http.post(`/pagination/next-one`, { entity, value: +value, code });
 };
 
 export const getPreviousOne = async (entity, value, columns = ["*"], code) => {
- return http.post(`pagination//previous-one`, {
-    entity,
-    value,
-    code,
-  });
+  getNextOne(entity, value, columns, code);
+  return http.post(`/pagination/previous-one`, { entity, value: +value, code });
 };
 
 export const getFirstOne = async (entity, columns = ["*"], code) => {
-  return http.post(`pagination//first-one`, {
-    entity,
-    code,
-  });
+  let params = {
+    entity
+  }
+  if (code) params.code = code;
+  return http.post(`/pagination/first-one`, { ...params });
 };
 
 export const getLastOne = async (entity, columns = ["*"], code) => {
-  return http.post(`pagination/last-one`, {
-    entity,
-    code,
-  });
+  console.log(`entity: ${entity}, columns: ${columns}, code: ${code}`);
+
+  let params = {
+    entity: 'bank',
+  }
+  if (code) params.code = code;
+  return http.post(`/pagination/last-one`, { ...params });
 };
 
