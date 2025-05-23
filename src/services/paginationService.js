7791d5ca1ecdafc90da4b7ffd0b8d205badaa159
mos3addev <mos3addev@gmail.com>
2025-05-23 19:00:39 +0300

    Merge branch 'main' of https://github.com/AymanEldawy/mateistate-client-vite-v2

--- src/services/paginationService.js
+++ src/services/paginationService.js
@@ -1,24 +1,55 @@
 import http from "./http";
 import API_URL_CONSTANTS from "./APIUrlConstants";
 
-export const getOneBy = async (entity, value, column = "number") => {
-  return http.post(`/pagination/one-by`, { entity, value, column });
-};
+export const getOneBy = async (entity, value, column = "id", columns = ["*"]) => {
+  const params = new URLSearchParams();
+  if (entity) params.append("entity", entity);
+  if (value) params.append("value", value);
+  if (column) params.append("column", column);
+  if (columns.length) params.append("columns", columns.join(","));
 
-export const getNextOne = async (entity, value, columns = ["*"], code) => {
-  return http.post(`/pagination/next-one`, { entity, value: +value, code });
+  return http.get(`${API_URL_CONSTANTS.BASE_ACCOUNT}?${params.toString()}`);
 };
-
-export const getPreviousOne = async (entity, value, columns = ["*"], code) => {
-  return http.post(`/pagination/previous-one`, { entity, value: +value, code });
+return http.post(`pagination/previous-one`, {
+  entity,
+  value,
+  code,
+});
 };
 
 export const getFirstOne = async (entity, columns = ["*"], code) => {
+return http.post(`pagination/first-one`, {
+  entity,
+  code,
+});
 
-  return http.post(`/pagination/first-one`, { entity, code });
+export const getNextOne = async (entity, value, columns = ["*"], code) => {
+   return http.post(`pagination/next-one`, {
+    entity,
+    value,
+    code,
+  });
+};
+
+export const getPreviousOne = async (entity, value, columns = ["*"], code) => {
+ return http.post(`pagination/previous-one`, {
+    entity,
+    value,
+    code,
+  });
+};
+
+export const getFirstOne = async (entity, columns = ["*"], code) => {
+  return http.post(`pagination/first-one`, {
+    entity,
+    code,
+  });
 };
 
 export const getLastOne = async (entity, columns = ["*"], code) => {
-  return http.post(`/pagination/last-one`, { entity, code });
+  return http.post(`pagination/last-one`, {
+    entity,
+    code,
+  });
 };
 
