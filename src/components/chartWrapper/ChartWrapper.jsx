ebf82c42c98791462c1b423bedd3832afe949299
Ayman Eldawy <aymaneldawy@yahoo.com>
2025-06-05 13:39:14 +0300

    working on chart

--- src/components/chartWrapper/ChartWrapper.jsx
+++ src/components/chartWrapper/ChartWrapper.jsx
@@ -1,12 +1,12 @@
+import { toTree } from '@/data/constants'
+import { usePopupForm } from '@/hook/usePopupForm'
 import { useQuery } from '@tanstack/react-query'
 import { useTranslation } from 'react-i18next'
-import Loading from '../shared/Loading'
-import ChartRenderTree from './ChartRenderTree'
+import { toast } from 'react-toastify'
 import { PaperHeader } from '../layout/paper/PaperHeader'
 import { ErrorText } from '../shared/ErrorText'
-import { toast } from 'react-toastify'
-import { usePopupForm } from '@/hook/usePopupForm'
-import { toTree } from '@/data/constants'
+import Loading from '../shared/Loading'
+import ChartRenderTree from './ChartRenderTree'
 
 const ChartWrapper = ({
   title,
@@ -15,16 +15,15 @@ const ChartWrapper = ({
   name,
   onSelectItem,
   onDeleteItem,
-  onClickAdd,
   onAddItemHandler
 }) => {
   const { t } = useTranslation()
   const { handleDispatchForm } = usePopupForm()
+
   const { isLoading, isError, data, error, isFetching, refetch } = useQuery({
     queryKey: [queryKey, 'chartTree'],
     queryFn: async () => {
       const response = await queryFn()
-
       if (response?.success) {
         return toTree(response?.data || [])
         // return response?.data || []
@@ -41,6 +40,8 @@ const ChartWrapper = ({
 
   // onUpdateItemHandler
   const onUpdateItemHandler = async (item) => {
+    console.log(item, '----');
+
     handleDispatchForm({
       table: name,
       oldValues: item,
